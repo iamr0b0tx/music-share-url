c905a62282b9cfd99602cad4a22f540e0295cb6b
iamr0b0x <abduladeneye@gmail.com>
2022-09-22 03:45:25 +0100

    working prototype for spotify

--- main.py
+++ main.py
@@ -1,49 +1,42 @@
-import requests
-from bs4 import BeautifulSoup
+from typing import Literal
+
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from utils import get_youtube_music_track_details, get_apple_music_track_details, get_spotify_track_details, \
+    get_spotify_track_url
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-def get_spotify_track_details(spotify_url):
-    """ takes url and return song title and artist name """
-    response = requests.get(spotify_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.find("meta", property="og:title")
-    description = soup.find("meta", property="og:description")
+@app.get("/urls")
+async def get_urls(platform: Literal["apple-music", "spotify", "youtube-music"], url: str):
+    try:
+        title, artist = {
+            "apple-music": get_apple_music_track_details,
+            "youtube-music": get_youtube_music_track_details,
+            "spotify": get_spotify_track_details,
+        }[platform](url)
 
-    title = title and title["content"]
-    artist = description and description["content"].split(" · Song · ")[0].strip()
-    return title, artist
+        urls = {}
+        for service in ["apple-music", "spotify", "youtube-music"]:
+            if platform == service:
+                continue
 
+            urls[service] = {
+                "apple-music": get_spotify_track_url,
+                "youtube-music": get_spotify_track_url,
+                "spotify": get_spotify_track_url,
+            }[service](title, artist)
 
-def get_apple_music_track_details(apple_music_url):
-    """ takes url and return song title and artist name """
-    response = requests.get(apple_music_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.find("meta", attrs={"name": "apple:title"})
-    title = title and title["content"]
-    description = soup.find("meta", property="og:title")
-    artist = title and description["content"].replace(f"{title} by", "", 1).strip()
-    return title, artist
+    except Exception:
+        raise HTTPException(status_code=404, detail="Item not found")
 
-
-def get_youtube_music_track_details(yt_music_url):
-    """ takes url and return song title and artist name """
-    response = requests.get(yt_music_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    title = soup.find("meta", property="og:title")
-    video_tags = soup.findAll("meta", property="og:video:tag")
-    title_tag = title and next(filter(lambda x: x["content"] in title["content"], video_tags), None)
-    title = title_tag and title_tag["content"]
-    artist = video_tags and video_tags[0]["content"]
-    return title, artist
+    return urls
 
 
-if __name__ == '__main__':
-    # url = "https://open.spotify.com/track/2Ka2eZtnllhJ8hukVLkXmt?si=RjLJTg0pTXOtZ1hWN-YGng&context=spotify%3Astation%3Atrack%3A60OEb5MtEmmepJPUJtYvFW"
-    url = "https://open.spotify.com/track/7o2CTH4ctstm8TNelqjb51?si=e03a989b1c034a26"
-    print(get_spotify_track_details(url))
-
-    url = "https://music.apple.com/ng/album/sweet-child-o-mine/1377826053?i=1377826892"
-    print(get_apple_music_track_details(url))
-
-    url = "https://music.youtube.com/watch?v=oMfMUfgjiLg&feature=share"
-    print(get_youtube_music_track_details(url))
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    with open("templates/index.html") as f:
+        return HTMLResponse(content=f.read(), status_code=200)
---
+++ utils.py
@@ -0,0 +1,89 @@
+from urllib.parse import quote
+
+import requests
+from bs4 import BeautifulSoup
+from decouple import config
+from requests.auth import HTTPBasicAuth
+
+
+CLIENT_ID = config("CLIENT_ID")
+CLIENT_SECRET = config("CLIENT_SECRET")
+
+
+def get_spotify_track_details(spotify_url):
+    """ takes url and return song title and artist name """
+    response = requests.get(spotify_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.find("meta", property="og:title")
+    description = soup.find("meta", property="og:description")
+
+    title = title and title["content"]
+    artist = description and description["content"].split(" · Song · ")[0].strip()
+    return title, artist
+
+
+def get_apple_music_track_details(apple_music_url):
+    """ takes url and return song title and artist name """
+    response = requests.get(apple_music_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.find("meta", attrs={"name": "apple:title"})
+    title = title and title["content"]
+    description = soup.find("meta", property="og:title")
+    artist = title and description["content"].replace(f"{title} by", "", 1).strip()
+    return title, artist
+
+
+def get_youtube_music_track_details(yt_music_url):
+    """ takes url and return song title and artist name """
+    response = requests.get(yt_music_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    title = soup.find("meta", property="og:title")
+    video_tags = soup.findAll("meta", property="og:video:tag")
+    title_tag = title and next(filter(lambda x: x["content"] in title["content"], video_tags), None)
+    title = title_tag and title_tag["content"]
+    artist = video_tags and video_tags[0]["content"]
+    return title, artist
+
+
+def get_auth_credentials():
+    response = requests.post(
+        f"https://accounts.spotify.com/api/token", auth=HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET),
+        data={"grant_type": "client_credentials"}, headers={"Content-Type": "application/x-www-form-urlencoded"}
+    )
+    return response.json()
+
+
+def get_auth_token():
+    return get_auth_credentials()["access_token"]
+
+
+def search_spotify(track_name, artist_name):
+    q = quote(f"{track_name} track:{track_name} artist:{artist_name}")
+    response = requests.get(
+        f"https://api.spotify.com/v1/search?type=track&include_external=audio&limit=1&q={q}",
+        headers={
+            "Authorization": f"Bearer {get_auth_token()}",
+            "Content-Type": "application/json"
+        }
+    )
+    return response.json()
+
+
+def get_spotify_track_url(track_name, artist_name):
+    track_info = search_spotify(track_name, artist_name)
+    return track_info["tracks"]["items"][0]["external_urls"]["spotify"]
+
+
+if __name__ == '__main__':
+    # url = "https://open.spotify.com/track/2Ka2eZtnllhJ8hukVLkXmt?si=RjLJTg0pTXOtZ1hWN-YGng&context=spotify%3Astation%3Atrack%3A60OEb5MtEmmepJPUJtYvFW"
+    # url = "https://open.spotify.com/track/7o2CTH4ctstm8TNelqjb51?si=e03a989b1c034a26"
+    # print(get_spotify_track_details(url))
+    #
+    # url = "https://music.apple.com/ng/album/sweet-child-o-mine/1377826053?i=1377826892"
+    # print(get_apple_music_track_details(url))
+    #
+    url = "https://music.youtube.com/watch?v=oMfMUfgjiLg&feature=share"
+    # print(get_youtube_music_track_details(url))
+
+    get_auth_token()
+    print(get_spotify_track_url(*get_youtube_music_track_details(url)))
